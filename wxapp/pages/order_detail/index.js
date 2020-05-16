60f4560465f5e28eaa8b1c0e0fcb10276d4dff97
lambo <lambo_computer>
2020-05-17 00:12:18 +0800

    Asdf

--- wxapp/pages/order_detail/index.js
+++ wxapp/pages/order_detail/index.js
@@ -5,15 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-    order: wx.getStorageSync("order_detail")
+    order: ''
   },
   onLoad: async function (options) {
-    this.setData({
-      order: {...wx.getStorageSync("order_detail"),...wx.getStorageSync("gift")}
-    });
-    setTimeout(() => {
-      console.log(this.data.order);
-    }, 200)
+    let order = wx.getStorageSync("order");
+    if ( order.picture) {
+      let pic = order.picture.split("\\");
+      order.pic = "http://localhost:5000/" + pic[pic.length-1].replace("bs-service/frontend/public", "");
+    }
+    if (order.picture_logo) {
+      let picture_logo = order.picture_logo.split("\\");
+      order._picture_logo = "http://localhost:5000/" + picture_logo[picture_logo.length-1].replace("bs-service/frontend/public", "");
+    }
+    order.specification = JSON.parse(order.specification);
+    order.custom_made = JSON.parse(order.custom_made);
+    order.user_address_json = JSON.parse(order.user_address_json);
+    console.log(order);
+    this.setData({order});
   },
 
 })
